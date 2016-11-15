42c2bb18ddaaa3e3ca7bc768c6c0666b08156994
Alexander Iundin <dorian.adret@gmail.com>
2016-11-16 00:16:47 +0300

    Add prototype of header and footer

--- src/BadBoysBoating/wwwroot/js/site.js
+++ src/BadBoysBoating/wwwroot/js/site.js
@@ -1,1 +1,27 @@
 ﻿// Write your Javascript code.
+window.onload = function () {
+    $("#menu_button").on("click", function () {
+        menuShowHide();
+    });
+}
+
+function menuShowHide() {
+    if ($("header").css("display") == "none") {
+        $("header").show();
+        $("footer").show();
+        $("#side_container").css("width", "20%");
+        $("#menu_button").css("margin-right", "5%");
+        $("#menu_button span").removeClass("glyphicon-chevron-right");
+        $("#menu_button span").addClass("glyphicon-chevron-left");
+        $(".body-content").css("width", "75%");
+    }
+    else {
+        $("header").hide();
+        $("footer").hide();
+        $("#side_container").css("width", "4%");
+        $("#menu_button").css("margin-right", "43%");
+        $("#menu_button span").removeClass("glyphicon-chevron-left");
+        $("#menu_button span").addClass("glyphicon-chevron-right");
+        $(".body-content").css("width", "95%");
+    }
+}
