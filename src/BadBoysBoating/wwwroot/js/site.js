5550cd89cafde1a18c900b4c1c68a820a4dce5b5
Alexander Iundin <dorian.adret@gmail.com>
2016-11-19 01:08:46 +0300

    Add dialog window for authorization and registration

--- src/BadBoysBoating/wwwroot/js/site.js
+++ src/BadBoysBoating/wwwroot/js/site.js
@@ -3,12 +3,26 @@ window.onload = function () {
     $("#menu_button").on("click", function () {
         menuShowHide();
     });
+    $("#login_button").on("click", function () {
+        showLoginWindow();
+    });
+    $("#logout_button").on("click", function () {
+        logout();
+    });
+    $("#register_button").on("click", function () {
+        showRegisterWindow();
+    });
+    $("#about_link").on("click", function () {
+        $("#about_list").collapse('toggle');
+    });
+    $("#register").on("click", function () {
+
+    });
 }
 
 function menuShowHide() {
     if ($("header").css("display") == "none") {
-        $("header").show();
-        $("footer").show();
+        $("header, footer").fadeToggle("slow");
         $("#side_container").css("width", "20%");
         $("#menu_button").css("margin-right", "5%");
         $("#menu_button span").removeClass("glyphicon-chevron-right");
@@ -16,12 +30,29 @@ function menuShowHide() {
         $(".body-content").css("width", "75%");
     }
     else {
-        $("header").hide();
-        $("footer").hide();
+        $("header, footer").hide();
         $("#side_container").css("width", "4%");
         $("#menu_button").css("margin-right", "43%");
         $("#menu_button span").removeClass("glyphicon-chevron-left");
         $("#menu_button span").addClass("glyphicon-chevron-right");
         $(".body-content").css("width", "95%");
     }
+}
+
+function showLoginWindow() {
+    $('#login_window').modal('show');
+    $("body").css('padding-right', '10%');   
+}
+
+function showRegisterWindow() {
+    $('#register_window').modal('show');
+    $("body").css('padding-right', '10%');
+}
+
+function login() {
+    
+}
+
+function logout() {
+    
 }
