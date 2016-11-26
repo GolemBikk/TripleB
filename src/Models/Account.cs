7f72a3185e2412f3d86a7119c073e23bcd30d846
Alexander Iundin <dorian.adret@gmail.com>
2016-11-26 23:03:22 +0300

    Add properties for libraries

--- src/Models/Account.cs
+++ src/Models/Account.cs
@@ -11,6 +11,8 @@ namespace Models
 
         public string password { get; set; }
 
+        public string token { get; set; }
+
         public string first_name { get; set; }
 
         public string last_name { get; set; }
