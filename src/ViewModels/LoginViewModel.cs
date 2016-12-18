ba3c0b0c932e5c9ae812b8a414efb28b1a535346
Alexander Iundin <dorian.adret@gmail.com>
2016-12-18 12:31:34 +0300

    Hotfix

--- src/ViewModels/LoginViewModel.cs
+++ src/ViewModels/LoginViewModel.cs
@@ -7,12 +7,8 @@ namespace ViewModels
         /// <summary>
         /// Уникальное имя пользователя
         /// </summary>
-<<<<<<< .merge_file_a03844
-        public string UserLogin { get; set; }
-=======
         [RegularExpression(@"[A-Za-z0-9]{4,20}", ErrorMessage = "Некорректный логин")]
-        public string Login { get; set; }
->>>>>>> .merge_file_a08536
+        public string UserLogin { get; set; }
 
         /// <summary>
         /// Пароль аккаунта
--- src/ViewModels/RegisterViewModel.cs
+++ src/ViewModels/RegisterViewModel.cs
@@ -7,22 +7,14 @@ namespace ViewModels
         /// <summary>
         /// Фамилия, Имя пользователя 
         /// </summary>
-<<<<<<< HEAD
-        public string UserName { get; set; }
-=======
         [RegularExpression(@"[A-Z][a-z][А-Я][а-я]+\s+[A-Z][a-z][А-Я][а-я]", ErrorMessage = "Некорректный адрес")]
-        public string Username { get; set; }
->>>>>>> 05c81c807fed0d29d04e7b2ba84f5ab48ac87bec
+        public string UserName { get; set; }
 
         /// <summary>
         /// Уникальное имя пользователя
         /// </summary>
-<<<<<<< HEAD
-        public string UserLogin { get; set; }
-=======
         [RegularExpression(@"[A-Za-z0-9]{4,20}", ErrorMessage = "Некорректный логин")]
-        public string Login { get; set; }
->>>>>>> 05c81c807fed0d29d04e7b2ba84f5ab48ac87bec
+        public string UserLogin { get; set; }
 
         /// <summary>
         /// Пароль аккаунта
