9015a0ff3a743eb41397434fb2714a1b0f2924bd
Michael Velichko <varian913@gmail.com>
2016-12-20 23:13:13 +0300

    Add Validation for BoatView and NewsView

--- src/ViewModels/BoatViewModel.cs
+++ src/ViewModels/BoatViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,6 +21,7 @@ namespace ViewModels
         /// <summary>
         /// Название лодки
         /// </summary>
+        [RegularExpression(@"[A-Z][a-z][А-Я][а-я]", ErrorMessage = "Некорректное название судна")]
         public String BoatModel { get; set; }
 
         /// <summary>
@@ -30,16 +32,19 @@ namespace ViewModels
         /// <summary>
         /// Полное описание лодки
         /// </summary>
+        [Required(ErrorMessage = "Нет описания судна")]
         public String Discription { get; set; }
 
         /// <summary>
         /// Стоимость лодки
         /// </summary>
+        [RegularExpression(@"[0-9]", ErrorMessage = "Некорректная стоимость судна")]
         public int Coast { get; set; }
 
         /// <summary>
         /// Крейсерская скорость лодки
         /// </summary>
+        [RegularExpression(@"[0-9]", ErrorMessage = "Некоррректная скорость судна")]
         public int Speed { get; set; }
 
         /// <summary>
@@ -50,21 +55,25 @@ namespace ViewModels
         /// <summary>
         /// Тип лодки (Яхта/Катер)
         /// </summary>
+        [RegularExpression(@"[ЯхтаКатер]", ErrorMessage = "Некорректный статус")]
         public String Type { get; set; }
 
         /// <summary>
         /// Длинна лодки
         /// </summary>
+        [RegularExpression(@"[0-9]", ErrorMessage = "Некоррректная длинна судна")]
         public int Lenght { get; set; }
 
         /// <summary>
         /// Ширина лодки
         /// </summary>
+        [RegularExpression(@"[0-9]", ErrorMessage = "Некоррректная ширина судна")]
         public int Width { get; set; }
 
         /// <summary>
         /// Водоизмещение лодки
         /// </summary>
+        [RegularExpression(@"[0-9]", ErrorMessage = "Некоррректное водоизмещение судна")]
         public int Displacement { get; set; }
 
         /// <summary>
