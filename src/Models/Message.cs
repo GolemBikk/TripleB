5d26ed1d6d9af17fc926822f28b7c439f7490a14
Alexander Iundin <dorian.adret@gmail.com>
2016-12-21 18:59:28 +0300

    Hotfix

---
+++ src/Models/Message.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    public class Message
+    {
+        /// <summary>
+        /// Primary key
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Дата подачи
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// От кого. Foreign key
+        /// </summary>
+        public int FromId { get; set; }
+
+        /// <summary>
+        /// Кому. Foreign key
+        /// </summary>
+        public int ToId { get; set; }
+
+        /// <summary>
+        /// Интересующая лодка. Foreign key
+        /// </summary>
+        public int BoatId { get; set; }
+    }
+}
